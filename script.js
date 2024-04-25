7bb4573832a13c83f3e6902655626a77973c8054
PedroHSPV <pedrohspv@gmail.com>
2024-04-25 15:47:28 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,9 @@
+// Este trecho de código será executado quando o DOM estiver completamente carregado
+document.addEventListener("DOMContentLoaded", function() {
+    // Aqui, estamos obtendo a referência para o elemento do logotipo com o ID "logo"
+    var logo = document.getElementById("logo");
+
+    // Adicionamos a classe "fade-in" ao elemento do logotipo
+    // Isso adiciona um efeito de fade-in ao elemento, controlado pelas regras CSS da classe "fade-in"
+    logo.classList.add("fade-in");
+});
