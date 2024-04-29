e0757d45e5d0029a0db7442b6eeaad7e4ff56b8a
PedroHSPV <pedrohspv@gmail.com>
2024-04-29 14:10:44 -0300

    commit

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Este trecho de código será executado quando o DOM estiver completamente carregado
-document.addEventListener("DOMContentLoaded", function() {
-    // Aqui, estamos obtendo a referência para o elemento do logotipo com o ID "logo"
-    var logo = document.getElementById("logo");
-
-    // Adicionamos a classe "fade-in" ao elemento do logotipo
-    // Isso adiciona um efeito de fade-in ao elemento, controlado pelas regras CSS da classe "fade-in"
-    logo.classList.add("fade-in");
-});
-// JavaScript para exibir/ocultar os links ao clicar no ícone do menu
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('active');
-});
 
 let slideIndex = 0;
 
